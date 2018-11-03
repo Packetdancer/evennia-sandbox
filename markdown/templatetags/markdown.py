aee2f413b3cff20b50df95c7425a70d86aff59a3
Rachel Blackman <seattlesparks@mac.com>
2018-11-02 21:49:26 -0700

    Fix Markdown needing to parse certain Evennia escape sequences.

--- markdown/templatetags/markdown.py
+++ markdown/templatetags/markdown.py
@@ -1,6 +1,6 @@
 from django import template
 from django.utils.html import urlize
-import markdown2
+from ..parser import MarkdownParser
 import re
 
 register = template.Library()
@@ -10,15 +10,15 @@ register = template.Library()
 def markdown(value):
     if not value:
         return value
-    return markdown2.markdown(value)
+
+    md = MarkdownParser(value)
+
+    return md.as_html()
 
 
 @register.filter
 def safe_urlize(text, trim_url_limit=None, nofollow=False, autoescape=False):
     result = urlize(text, trim_url_limit=trim_url_limit, nofollow=nofollow, autoescape=autoescape)
-
-    if re.search(r'<a href="<a href="([^\"]*)">([^\<]*)</a>">', result):
-        print("Matched!")
 
     result = re.sub(r'<a href="<a href="([^\"]*)">([^\<]*)</a>">',
                     r'<a href="\1">', result)
