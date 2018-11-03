06fa659af42ac775bebf32f78292c8fd99b0854c
Rachel Blackman <seattlesparks@mac.com>
2018-11-02 21:27:11 -0700

    Make a new urlize filter variant so Markdown links work on web.

--- markdown/templatetags/markdown.py
+++ markdown/templatetags/markdown.py
@@ -1,5 +1,7 @@
 from django import template
+from django.utils.html import urlize
 import markdown2
+import re
 
 register = template.Library()
 
@@ -9,3 +11,16 @@ def markdown(value):
     if not value:
         return value
     return markdown2.markdown(value)
+
+
+@register.filter
+def safe_urlize(text, trim_url_limit=None, nofollow=False, autoescape=False):
+    result = urlize(text, trim_url_limit=trim_url_limit, nofollow=nofollow, autoescape=autoescape)
+
+    if re.search(r'<a href="<a href="([^\"]*)">([^\<]*)</a>">', result):
+        print("Matched!")
+
+    result = re.sub(r'<a href="<a href="([^\"]*)">([^\<]*)</a>">',
+                    r'<a href="\1">', result)
+
+    return result
